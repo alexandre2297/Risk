a07d5491cc7912adbe0a3751ec44bdee4710f056
alexandre2297 <alexandre28200@gmail.com>
2019-06-05 11:25:43 +0200

    update move and add class Triple

--- src/IA/Move.java
+++ src/IA/Move.java
@@ -1,7 +1,19 @@
 package IA;
 
+import Game.Country;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class Move {
+    public List<Pair<Integer,Country>> placementList;
+    public List<Triple<Integer,Country,Country>> attackList;
+    public List<Triple<Integer,Country,Country>> renforcementList;
+
+    public Move() {
+        placementList = new ArrayList<>();
+        attackList = new ArrayList<>();
+        renforcementList = new ArrayList<>();
+    }
 
 }
